0cfca86f7bcb32b98175eb845086c0877d816bdd
Martin Schlodinski <martin.schlodinski@moia.io>
2025-01-11 20:44:51 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+
+<script setup>
+import { SerialPort } from 'serialport';
+
+new SerialPort('COM1', { baudRate: 9600 });
+</script>
